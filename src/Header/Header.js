61d5b9c701e7858f3aa6e6dc9edf0acd8d40d66d
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-07-06 15:40:47 -0300

    Add header animation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,7 @@ export function Header() {
     <>
       <h1>
         30 days of React
-        <img src={atom} alt="react" />
+        <img src={atom} alt="react" className="spin" />
       </h1>
       <h2>Day 5</h2>
     </>
